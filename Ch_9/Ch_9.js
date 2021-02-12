63b68a7188d312397131631f0775f02056ee845e
asma-jahangeer <57453580+asma-jahangeer@users.noreply.github.com>
2021-02-13 00:07:07 +0500

    Add files via upload

---
+++ Ch_9/Ch_9.js
@@ -0,0 +1,4 @@
+var fName = prompt("Enter first name");
+var lName = prompt("Enter last name");
+var age = prompt("Enter your age");
+var greetingMessage = alert("Hello " +fName + lName + " You are  "+age+ " years old");
