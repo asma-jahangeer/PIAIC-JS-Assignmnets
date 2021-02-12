4dabfdc7c3f017e4f967cf8f11c5cd72e603d0ae
asma-jahangeer <57453580+asma-jahangeer@users.noreply.github.com>
2021-02-12 23:51:36 +0500

    Add files via upload

---
+++ Chapter_1_2_3/Chap-1-2-3.js
@@ -0,0 +1,7 @@
+var firstName = "Amsa";
+var lastName = "Jahangeer";
+
+var age = 20;
+var alertMessage = "Hello" + " " + firstName +  " " + lastName + "," + " "+ "You are" + " " +
+age + " " + "years" + " " +  "old";
+alert(alertMessage);
